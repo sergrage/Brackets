4ea31f751a81383063f9a2d15766699609bc7015
Sergey Shmatovskiy <elenergetica87@mail.ru>
2018-08-09 12:16:23 +0300

    add analysis function in Brackets class

--- src/Brackets.php
+++ src/Brackets.php
@@ -1,14 +1,34 @@
 <?php
 
-namespace
-
 class Brackets {
 	
 
-	 public function analysis($value)
-	 {
-	 	
-	 }
+	public function analysis($value)
+	{
+		$symbols = [')', '(', "\n", "\r", "\t", ' '];
+		$len = strlen($value);
+		$result = [];
 
+		for($i = 0; $i < $len; $i++) {
+			if (!in_array($value[$i], $symbols)) {
+				throw new InvalidArgumentException('Переданы неправильные параметры');
+			}
 
+			if ($value[$i] == $symbols[1]) {
+				array_push($result, $value[$i]);
+			}
+
+			if ($value[$i] == $symbols[0] && end($result) == $symbols[1]) {
+				array_pop($result);
+			} elseif ($value[$i] == $symbols[0] && end($result) != $symbols[1]) {
+				array_push($result, $value[$i]);
+			} 
+		}
+
+		if(!$result) {
+			return true;
+		} 
+
+		return false;
+	}
 }
